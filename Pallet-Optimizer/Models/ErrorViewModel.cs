c80c6d1f725e5d2a669368735168145e4b2d744f
SomethingFishy <69714456+Ianisop@users.noreply.github.com>
2025-11-11 13:23:19 +0100

    init

---
+++ Pallet-Optimizer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pallet_Optimizer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
