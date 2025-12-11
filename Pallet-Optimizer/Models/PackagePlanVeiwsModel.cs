21ef32da0dbc9db9a6bbeb098fa7b0e19864e8a9
Chasi <magnus.bekhoj21221@gmail.com>
2025-12-11 10:55:41 +0100

    Reapply "Merge branch 'main' into KAN-49-Sprint-2-Database-integration-og-Framework"
    This reverts commit 31a94dafbfdfdd4ff7fd129e0776edac8dc43158.

---
+++ Pallet-Optimizer/Models/PackagePlanVeiwsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Pallet_Optimizer.Models
+{
+    public class PackagePlanViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public int PalletCount { get; set; }
+        public int TotalElements { get; set; }
+        public double TotalWeight { get; set; }
+    }
+}
